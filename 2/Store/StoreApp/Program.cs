cd60cc78e97d334a4c29179252991a3c6067d8be
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2023-07-28 15:49:12 +0300

    Commit

---
+++ 2/Store/StoreApp/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<RepositoryContext>(options => 
+{
+  options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+/* launchSettings.json under Properties tab
+"http": {
+      "commandName": "Project",
+      "dotnetRunMessages": true,
+      "launchBrowser": true,
+      "applicationUrl": "http://localhost:5049",
+      "environmentVariables": {
+        "ASPNETCORE_ENVIRONMENT": "Development"
+      }
+    },
+*/
