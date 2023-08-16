20f2432267e045573db2a214276cfc1cb9065f46
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2023-08-16 15:23:11 +0300

    commit

--- 2/Store/Repositories/RepositoryManager.cs
+++ 2/Store/Repositories/RepositoryManager.cs
@@ -6,16 +6,18 @@ namespace Repositories
     {   
         private readonly RepositoryContext _context;
         private readonly IProductRepository _productRepository;
-
-        public RepositoryManager(IProductRepository productRepository, RepositoryContext context)
+        private readonly ICategoryRepository _categoryRepository;
+        public RepositoryManager(IProductRepository productRepository, RepositoryContext context, ICategoryRepository categoryRepository)
         {
             _productRepository = productRepository;
             _context = context;
+            _categoryRepository = categoryRepository;
         }
 
         //public IRepositoryManager Product => throw new NotImplementedException();
         public IProductRepository Product => _productRepository;
-        
+
+        public ICategoryRepository Category => _categoryRepository;
 
         public void Save()
         {
