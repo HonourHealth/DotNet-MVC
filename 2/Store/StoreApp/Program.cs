a7de3df5cd0ca07a765903b788bb0bee35ece7f7
Onur Can <24491805+HonourHealth@users.noreply.github.com>
2023-10-23 16:22:45 +0300

    commit

--- 2/Store/StoreApp/Program.cs
+++ 2/Store/StoreApp/Program.cs
@@ -6,15 +6,15 @@ using Services.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<RepositoryContext>(options => 
+builder.Services.AddDbContext<RepositoryContext>(options =>
 {
   options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
   b => b.MigrationsAssembly("StoreApp"));
 });
 
-builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddScoped<IServiceManager, ServiceManager>();
 builder.Services.AddScoped<IProductService, ProductManager>();
@@ -25,10 +25,18 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+      name:"Admin",
+      areaName:"Admin",
+      pattern:"Admin/{controller=Dashboard}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
-app.MapControllerRoute(
-    name:"default",
-    pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
